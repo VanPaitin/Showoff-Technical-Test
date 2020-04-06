7d64a5187f4bece4cd05756a2f8a127de71aa567
Mayowa Pitan <mayowa.pitan@andela.com>
2020-04-06 04:15:41 +0100

    add axios request client

--- app/javascript/react/root.tsx
+++ app/javascript/react/root.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import axios from 'axios'
 
 export default class Root extends React.Component {
   componentDidMount() {
-
+    axios.get('/widgets/visible')
+      .then(console.log)
   }
   render() {
     return (
