729972cc4efc2a6372eaa84830b7d846501f2ae4
Mayowa Pitan <mayowa.pitan@andela.com>
2020-04-08 16:59:01 +0100

    extract navigation bar

---
+++ app/javascript/react/Navbar.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const StyledNav = styled(Nav)`
+  margin-left: auto;
+  &.navbar-nav .nav-link {
+    color: white;
+    text-transform: uppercase;
+  }
+`;
+
+export default ({ loggedIn, openModal }) => {
+  const openLoginModal = () => openModal('session')
+
+  const openSignUpModal = () => openModal('user')
+
+  const openChangePasswordModal = () => openModal('changePassword')
+
+  const openResetPasswordModal = () => openModal('resetPassword')
+
+  const openLogoutModal = () => openModal('logout')
+
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="/">Widgets</Navbar.Brand>
+
+      <StyledNav>
+        {loggedIn ? (
+          <>
+            <Nav.Link href="" onClick={openLogoutModal}>Logout</Nav.Link>
+            <Nav.Link href="" onClick={openChangePasswordModal}>Change Password</Nav.Link>
+          </>
+        ) : (
+          <>
+            <Nav.Link href="" onClick={openLoginModal}>Login</Nav.Link>
+            <Nav.Link href="" onClick={openResetPasswordModal}>Reset Password</Nav.Link>
+            <Nav.Link href="" onClick={openSignUpModal}>Register</Nav.Link>
+          </>
+        )}
+      </StyledNav>
+    </Navbar>
+  )
+}
--- app/javascript/react/forms/UserForm.tsx
+++ app/javascript/react/forms/UserForm.tsx
@@ -8,54 +8,49 @@ const Label = styled(Form.Label)`
   font-weight: bold;
 `;
 
-export default ({ validated }) => {
-  let token = document.querySelector<HTMLMetaElement>('meta[name="csrf-token"]').content;
-
-  return (
-    <Form noValidate validated={validated}>
-      <Form.Control as='input' type='hidden' name='authenticity_token' value={token}/>
-      <Form.Row>
-        <Form.Group as={Col}>
-          <Label>First name</Label>
-          <Form.Control placeholder="First name" name='[user]first_name' type='text' required/>
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type='invalid'>
-            Please enter your first name!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Label>Last name</Label>
-          <Form.Control placeholder="Last name" name='[user]last_name'/>
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-
-      <Form.Group controlId="formBasicEmail">
-        <Label>Email address</Label>
-        <Form.Control type="email" placeholder="Enter email" name='[user]email' required/>
+export default ({ validated }) =>
+  <Form noValidate validated={validated}>
+    <Form.Row>
+      <Form.Group as={Col}>
+        <Label>First name</Label>
+        <Form.Control placeholder="First name" name='[user]first_name' type='text' required/>
         <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         <Form.Control.Feedback type='invalid'>
-            Please enter your email!
-          </Form.Control.Feedback>
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+          Please enter your first name!
+        </Form.Control.Feedback>
       </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-        <Label>Password</Label>
-        <Form.Control type="password" placeholder="Password" name='[user]password' required/>
+      <Form.Group as={Col}>
+        <Label>Last name</Label>
+        <Form.Control placeholder="Last name" name='[user]last_name'/>
         <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        <Form.Control.Feedback type='invalid'>
-            Please enter a password!
-          </Form.Control.Feedback>
       </Form.Group>
-      <Form.Group controlId="formBasicFile">
-        <FormFile>
-          <Label>Avatar</Label>
-          <FormFile.Input name='file'/>
-        </FormFile>
-      </Form.Group>
-    </Form>
-  )
-}
+    </Form.Row>
+
+    <Form.Group controlId="formBasicEmail">
+      <Label>Email address</Label>
+      <Form.Control type="email" placeholder="Enter email" name='[user]email' required/>
+      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      <Form.Control.Feedback type='invalid'>
+          Please enter your email!
+        </Form.Control.Feedback>
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Label>Password</Label>
+      <Form.Control type="password" placeholder="Password" name='[user]password' required/>
+      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      <Form.Control.Feedback type='invalid'>
+          Please enter a password!
+        </Form.Control.Feedback>
+    </Form.Group>
+    <Form.Group controlId="formBasicFile">
+      <FormFile>
+        <Label>Avatar</Label>
+        <FormFile.Input name='file'/>
+      </FormFile>
+    </Form.Group>
+  </Form>
+
