44ee8a3c0dad49163bb6562d9cade33fe828f060
Mayowa Pitan <mayowa.pitan@andela.com>
2020-04-08 16:50:44 +0100

    enable webpack to use env variables

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const dotenv = require('dotenv')
+const webpack = require('webpack')
 const typescript =  require('./loaders/typescript')
 
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
+environment.plugins.prepend('Environment', new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env))))
 environment.loaders.prepend('typescript', typescript)
+
 module.exports = environment
